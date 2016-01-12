668ac689c70c3525cc2fd0b872437762bc3a9cb2
Brett Kercher <brettkercher@utexas.edu>
2016-01-12 17:49:24 -0600

    Implementing flask backend

---
+++ users.py
@@ -0,0 +1,31 @@
+import flask.ext.login as flask_login
+
+users = {'foo@bar.tld': {'pw': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+#takes some sort of user id, and should return the user object.
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['pw'] == users[email]['pw']
+
+    return user
