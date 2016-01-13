e9734bca330c615b27e8854ea511f63ee3639622
Brett Kercher <brettkercher@utexas.edu>
2016-01-13 16:42:30 -0600

    giving up on windows

---
+++ schema.py
@@ -0,0 +1,10 @@
+from app import app, db
+from models import User
+
+with app.app_context():
+	db.create_all()
+	admin = User('admin', 'admin@example.com')
+	guest = User('guest', 'guest@example.com')
+	db.session.add(admin)
+	db.session.add(guest)
+	db.session.commit()
